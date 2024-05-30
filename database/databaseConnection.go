658dc407a5b87bcecd982c9f1e340908dec6b86c
Divyanshu050303 <divyanshusingh7060@gmail.com>
2024-05-30 09:54:06 +0530

    database connection

---
+++ database/databaseConnection.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDb := "mongodb://localhost:27017"
+	fmt.Print(MongoDb)
+
+	client, error := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	ctx, cancle := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancle()
+
+	err := client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb")
+	return client
+
+}
+
+var Client *mongo.Client = DBinstance()
+
+func openCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+
+	var collection *mongo.Collection = client.Database("rastaurant").Collection(collectionName)
+	return collection
+
+}
